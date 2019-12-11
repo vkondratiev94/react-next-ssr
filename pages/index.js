a5c4d5c1427d5a092bcfc819b46632c498ee3af7
vkondratiev94 <vkondratiev94@gmail.com>
2019-12-11 12:30:43 +0200

    getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import axios from "axios";
 import MainLayout from "../components/layouts/mainLayout";
 
 export default class Home extends Component {
+  static async getInitialProps() {
+    let userData;
+
+    try {
+      const res = await axios.get(
+        "https://jsonplaceholder.typicode.com/users/1"
+      );
+      userData = res.data;
+    } catch {
+      console.log("error");
+    }
+
+    return {
+      user: {
+        name: "John",
+        lastname: "Doe"
+      },
+      userData
+    };
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: this.props.user,
+      userData: this.props.userData
+    };
+  }
+
   render() {
     return (
       <MainLayout>
