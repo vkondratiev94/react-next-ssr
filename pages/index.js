d7b6b97607199aef945386af65484cdba0e5de1f
vkondratiev94 <vkondratiev94@gmail.com>
2019-12-11 12:07:53 +0200

    pages and links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import Link from "next/link";
 
 export default class Home extends Component {
   render() {
     return (
       <>
+        <div>
+          <nav>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+
+            <Link href="/users">
+              <a>Users</a>
+            </Link>
+          </nav>
+        </div>
+
         <h1>Welcome to my page!</h1>
       </>
     );
