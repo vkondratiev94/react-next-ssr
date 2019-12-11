99ba6e0b7406f69a08047cf1251cc928d0a6653b
vkondratiev94 <vkondratiev94@gmail.com>
2019-12-11 16:01:58 +0200

    Router

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import MainLayout from "../components/layouts/mainLayout";
 import Message from "../components/includes/message";
+import Router from "next/router";
 
 export default class About extends Component {
+  componentDidMount() {
+    console.log(Router.pathname);
+    console.log(Router.query);
+
+    // called before going back from the page
+    Router.beforePopState(({ url, as, options }) => {
+      // if previous page is '/contact', skip coming back
+      if (as === "/contact") {
+        window.location.href = "/whatever";
+        return false;
+      }
+      return true;
+    });
+
+    Router.replace("/contact", "/contact/56");
+  }
+
   render() {
     return (
       <MainLayout>
         <h1>About</h1>
         <Message />
+        <br />
+        <span onClick={() => Router.push("/users/profile/1")}>
+          Click me now!
+        </span>
       </MainLayout>
     );
   }
