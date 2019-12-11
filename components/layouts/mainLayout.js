9026c4fe5dd71f03dccd718e7b7e30fc979ede24
vkondratiev94 <vkondratiev94@gmail.com>
2019-12-11 12:51:07 +0200

    working with styles and static files

--- components/layouts/mainLayout.js
+++ components/layouts/mainLayout.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Head from "next/head";
 import Header from "../includes/header";
 
 const MainLayout = props => {
   return (
     <>
-      <Header />
-      {props.children}
+      <Head>
+        <title>My Awesome App</title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link href="/static/css/styles.css" rel="stylesheet" />
+      </Head>
+
+      <div className="mainLayout container">
+        <Header />
+        {props.children}
+      </div>
     </>
   );
 };
--- pages/contact.js
+++ pages/contact.js
@@ -6,6 +6,19 @@ export default class Contact extends Component {
     return (
       <MainLayout>
         <h1>Contact</h1>
+        <div style={{ color: "red" }}>Hello guys</div>
+        <div className="jsxStyled">Styles with styled-jsx</div>
+        <div className="contact_static">Styles with styles.css</div>
+        <div>
+          <img src="/static/images/next.png" />
+        </div>
+
+        {/* built in styled-jsx */}
+        <style jsx>{`
+          .jsxStyled {
+            color: blue;
+          }
+        `}</style>
       </MainLayout>
     );
   }
